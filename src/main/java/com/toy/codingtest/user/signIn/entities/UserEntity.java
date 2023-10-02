08907b8c5466e75865cc1ae09f6516f6d0a5a390
logicdrive <logicdrive7@gmail.com>
2023-10-02 10:54:22 +0900

    Feat: 유저 관련 엔티티 생성

---
+++ src/main/java/com/toy/codingtest/user/signIn/entities/UserEntity.java
@@ -0,0 +1,26 @@
+package com.toy.codingtest.user.signIn.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "app_user")
+public class UserEntity {
+    @Id
+    @Column(nullable = false)
+    private String id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+}
