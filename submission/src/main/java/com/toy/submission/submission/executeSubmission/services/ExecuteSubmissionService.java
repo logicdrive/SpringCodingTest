6e7e6549c6846064dd273da799f1d39e456f7ed4
logicdrive <logicdrive7@gmail.com>
2023-11-09 13:08:42 +0900

    Feat: 디렉토리 평가 관련 디렉토리가 자동으로 생성되도록 만듬

--- submission/src/main/java/com/toy/submission/submission/executeSubmission/services/ExecuteSubmissionService.java
+++ submission/src/main/java/com/toy/submission/submission/executeSubmission/services/ExecuteSubmissionService.java
@@ -1,5 +1,7 @@
 package com.toy.submission.submission.executeSubmission.services;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
@@ -21,9 +23,33 @@ public class ExecuteSubmissionService {
         System.out.println("Current working directory: " + System.getProperty("user.dir"));
 
 
+        // submissions 와 관련된 폴더가 없을 경우 구성시키기 위해서
+        // 평가에 필요한 스크립트인 scripts/execute.py를 복사해서 가져오고, 폴더를 구성시켜줌
+        String submissionsPath = "submissions/";
+        File submissionDir = new File(submissionsPath);
+        if(!submissionDir.exists()) submissionDir.mkdir();
+
+        String submissionExecutePath = (submissionsPath + "execute.py");
+        File submissionsExecuteFile = new File(submissionExecutePath);
+        if(!submissionsExecuteFile.exists()) {
+            try {
+                
+                List<String> executeLines = Files.readAllLines(Paths.get("scripts/execute.py"));
+    
+                FileWriter executeFileWriter = new FileWriter(submissionExecutePath);
+                for (String line : executeLines) 
+                    executeFileWriter.write(line + "\n");
+                executeFileWriter.close();
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }  
+        }
+        
+
         // 주어진 정보를 기반으로 평가 스크립트인 execute.py가 이용할 수 있도록 디렉토리 경로를 구성하는 작업
         // 주어진 코드들을 격리시켜서 실행시키기 위해서 submissions/[UUID] 와 같은 디렉토리를 생성함
-        String submitDirPath = ("submissions/" + UUID.randomUUID().toString() + "/");
+        String submitDirPath = (submissionsPath + UUID.randomUUID().toString() + "/");
         File submitDir = new File(submitDirPath);
         submitDir.mkdir();
 
@@ -52,5 +78,16 @@ public class ExecuteSubmissionService {
             e.printStackTrace();
         }
 
+
+        // 평가 스크립트인 execute.py를 실행시켜서 주어진 Submission에 대한 처리를 하도록 만들기 위해서
+        // try {
+
+        //     String command = "python /path/to/your/python/file.py"; submissionExecutePath
+        //     Process process = Runtime.getRuntime().exec(command);
+        
+        // } catch (IOException e) {
+        //     System.out.println("An error occurred.");
+        //     e.printStackTrace();
+        // }
     }
 }
