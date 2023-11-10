787c1ae6774b514cba6aa27a8783ccf88816fb8e
logicdrive <logicdrive7@gmail.com>
2023-11-11 08:15:40 +0900

    Feat: 이동을 위한 Navigation Bar를 추가시킴

--- frontend/src/navigation/Navigation.js
+++ frontend/src/navigation/Navigation.js
@@ -1,5 +1,45 @@
+import AppBar from '@mui/material/AppBar';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@mui/material/Link';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
 const Navigation = () => {
-    return <h1>--- NAV ---</h1>
+    return (
+        <AppBar position="static">
+            <Container maxWidth="lg">
+                <Toolbar disableGutters>
+                    <Link component={RouterLink} to="/" variant="h5" underline="none" sx={{color: "white", fontWeight: "bolder", fontFamily: "BMDfont"}}>
+                        스프링&gt;코딩테스트
+                    </Link>
+
+                    <Link component={RouterLink} to="/problem/showing/showAll">
+                        <Button>
+                            <Typography sx={{color: "white", fontWeight: "bolder", fontFamily: "BMDfont", paddingLeft:2}}>문제풀기</Typography>
+                        </Button>
+                    </Link>
+                    <Link component={RouterLink} to="/problem/editing/showOnlyEditable" sx={{flexGrow:1}}>
+                        <Button>
+                            <Typography sx={{color: "white", fontWeight: "bolder", fontFamily: "BMDfont"}}>문제관리</Typography>
+                        </Button>
+                    </Link>
+   
+                    <Link component={RouterLink} to="/user/signin">
+                        <Button>
+                            <Typography sx={{color: "white", fontWeight: "bolder", fontFamily: "BMDfont"}}>로그인</Typography>
+                        </Button>
+                    </Link>
+                    <Link component={RouterLink} to="/user/signup">
+                        <Button>
+                            <Typography sx={{color: "white", fontWeight: "bolder", fontFamily: "BMDfont"}}>회원가입</Typography>
+                        </Button>
+                    </Link>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
 }
 
 export default Navigation;
