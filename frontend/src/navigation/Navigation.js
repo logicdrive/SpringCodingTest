8ce9ad7d357060087d44144759a8310acf94db10
logicdrive <logicdrive7@gmail.com>
2023-11-11 09:32:21 +0900

    Feat: 회원가입 페이지에 실제 값이 입력되고, 반영되도록 만듬

--- frontend/src/navigation/Navigation.js
+++ frontend/src/navigation/Navigation.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import { Link as RouterLink } from 'react-router-dom';
 import { Container, Toolbar, Link, Button, Typography } from '@mui/material';
--- frontend/src/user/signUp/SignUpPage.js
+++ frontend/src/user/signUp/SignUpPage.js
@@ -1,6 +1,30 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Container, Paper, Button, TextField, Typography } from '@mui/material';
 
 const SignUpPage = () => {
+    const navigate = useNavigate();
+    const [signUpData, setSignUpData] = useState({
+        email: "",
+        password: "",
+        name: "",
+    });
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        console.log(signUpData);
+    };
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setSignUpData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    
     return (
         <>
             <Typography sx={{color: "black", fontWeight: "bolder", fontFamily: "BMDfont", paddingTop:2}}>회원가입</Typography>
@@ -13,9 +37,14 @@ const SignUpPage = () => {
                     </Typography>
                     <hr style={{border: "solid 0.1px lightgray", opacity: "0.25"}}/>
 
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <TextField
                                 label="이메일"
+                                name="email"
+
+                                value={signUpData.email}
+                                onChange={handleInputChange}
+
                                 variant="standard"
                                 margin="normal"
                                 fullWidth
@@ -23,6 +52,12 @@ const SignUpPage = () => {
                             />
                             <TextField
                                 label="비밀번호"
+                                name="password"
+                                type="password"
+
+                                value={signUpData.password}
+                                onChange={handleInputChange}
+
                                 variant="standard"
                                 margin="normal"
                                 fullWidth
@@ -30,6 +65,11 @@ const SignUpPage = () => {
                             />
                             <TextField
                                 label="닉네임"
+                                name="name"
+
+                                value={signUpData.name}
+                                onChange={handleInputChange}
+
                                 variant="standard"
                                 margin="normal"
                                 fullWidth
