c1a82c0f52a37e12dcd87619f047ac8ccd6039a7
logicdrive <logicdrive7@gmail.com>
2023-11-11 16:07:46 +0900

    Feat: 새로고침시에도 로그인 상태가 유지되도록 만듬

--- frontend/src/components/jwtToken/JwtTokenContext.js
+++ frontend/src/components/jwtToken/JwtTokenContext.js
@@ -32,16 +32,23 @@ const JwtTokenContext = createContext();
 const JwtTokenProvider = ({ children }) => {
     const [jwtTokenState, dispatch] = useReducer(reducer, initialState);
 
+
     const registerTokenValue = (jwtToken) => {
         const tokenPayload = JSON.parse(atob(jwtToken.split(".")[1]))
-    
+        
+        localStorage.setItem("jwtToken", jwtToken);
         dispatch({type: REGISTER_TOKEN_VALUE, payload: {
             value:jwtToken, email:tokenPayload.sub, name:tokenPayload.name, Authorization:`Bearer ${jwtToken}`
         }})
     };
+    if((jwtTokenState.jwtToken == null) && (localStorage.getItem("jwtToken") != null))
+        registerTokenValue(localStorage.getItem("jwtToken"))
+
     const deleteTokenValue = () => {
+        localStorage.removeItem("jwtToken")
         dispatch({type: DELETE_TOKEN_VALUE, payload:null})
     };
+
 
     return (
         <JwtTokenContext.Provider value={{ jwtTokenState, registerTokenValue, deleteTokenValue }}>
