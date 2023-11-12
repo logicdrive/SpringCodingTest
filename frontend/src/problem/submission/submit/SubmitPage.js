37a149966fa1b5e07c90c42f140b1c41926500d9
logicdrive <logicdrive7@gmail.com>
2023-11-12 09:06:06 +0900

    Feat: 코드를 제출할 수 있게됨

--- frontend/src/problem/submission/submit/SubmitPage.js
+++ frontend/src/problem/submission/submit/SubmitPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { TextField, Button, MenuItem } from '@mui/material';
+import axios from 'axios';
+import APIConfig from "../../../APIConfig";
 import ProblemSubmissionNavigation from '../../navigation/ProblemSubmissionNavigation';
 import { JwtTokenContext } from "../../../components/jwtToken/JwtTokenContext";
 import { AlertPopupContext } from "../../../components/alertPopUp/AlertPopUpContext"
@@ -27,10 +29,16 @@ const SubmitPage = () => {
 
         try {
 
-            console.log(submissionInfo)
-            // await axios.post(`${APIConfig.url}/users`, signUpData);
-            // addAlertPopUp("회원가입이 성공적으로 완료되었습니다.", "success");
-            // navigate("/user/signin");
+            const requestHeader = {headers: {Authorization: jwtTokenState.jwtToken.Authorization}};
+            const submissionToSend = {
+                code: submissionInfo.code,
+                language: submissionInfo.language,
+                problemId: problemId
+            }
+            await axios.post(`${APIConfig.url}/submissionInfos/submissions`, submissionToSend, requestHeader);
+
+            addAlertPopUp("제출이 성공적으로 완료되었습니다.", "success");
+            navigate(`/problem/submission/showSubmissionAll/${problemId}`);
 
         } catch (error) {
             addAlertPopUp("제출 에러가 발생했습니다!", "error");
